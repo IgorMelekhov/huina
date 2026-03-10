cb00657b2eeab87260d02a058a699c714d924d35
Игорь Мелехов <igormelekhov403@gmail@gmail.com>
2026-03-10 09:38:06 +0300

    init

--- miro/miro/settings.py
+++ miro/miro/settings.py
@@ -36,7 +36,11 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'daphne',
     'django.contrib.staticfiles',
+    'miro_app',
+    'miro_api',
+    'channels',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +78,12 @@ WSGI_APPLICATION = 'miro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'socket',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
     }
 }
 
